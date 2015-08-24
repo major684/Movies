c55cbbc8fa10605a35e884b13c9f10744aefb331
major684 <lucas.velasquez@outlook.com>
2015-08-23 21:03:19 -0700

    Final Code files
    All code files that we need to generate the movies html page with our
    movie trailers.

--- media.py
+++ media.py
@@ -1,6 +1,20 @@
+#Movies Project
+#Title:Classes
+#Author: Lucas Velasquez
+
+#Import the webbrowser for the show_trailer function
+import webbrowser
+
+#Create a class Movie 
 class Movie():
+    #Initialize an instance of the class and give it properties or arguements
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        #Self refers to the instance when the class is invoked
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtub_url = trailer_youtube
+        self.trailer_youtube_url = trailer_youtube
+    #Creates a new function called show_trailer where we open a web browser to show the
+    #Trailer of the movie.
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
