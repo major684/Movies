99dc5deec6390c50cdec7e76dbe13ac58edc7fc0
major684 <lucas.velasquez@outlook.com>
2015-08-23 18:10:01 -0700

    python files
    created movie class

---
+++ entertainment_center.py
@@ -0,0 +1,12 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc");
+#print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+print(avatar.storyline)
